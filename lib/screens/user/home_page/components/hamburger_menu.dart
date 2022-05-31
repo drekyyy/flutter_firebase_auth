908c1cceb82c7387a50a7cac26bca51575ab3be2
Darek <heniulu10@gmail.com>
2022-05-31 18:16:10 +0200

    feat(home_page) add house id button in hamburger menu with pop up window to copy house id to clipboard (showdialog)

--- lib/screens/user/home_page/components/hamburger_menu.dart
+++ lib/screens/user/home_page/components/hamburger_menu.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:flutter_shopping_list/services/database_service.dart';
+import 'package:provider/provider.dart';
+import '../../../../models/user.dart';
 import '../../change_house_page/change_house_page.dart';
 import '../../settings_page/settings_page.dart';
 
@@ -8,6 +11,8 @@ class HamburgerMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //String houseId = '';
+    final String uid = Provider.of<SimpleUser?>(context)!.uid;
     return Drawer(
         child: ListView(
       children: [
@@ -25,13 +30,42 @@ class HamburgerMenu extends StatelessWidget {
         ListTile(
             iconColor: Colors.white,
             textColor: Colors.white,
-            leading: const Icon(Icons.house),
+            leading: const Icon(Icons.change_circle),
             title: const Text('Zmień dom'),
             onTap: () {
               Navigator.push(
                   context,
                   MaterialPageRoute(
                       builder: (context) => const ChangeHousePage()));
+            }),
+        ListTile(
+            iconColor: Colors.white,
+            textColor: Colors.white,
+            leading: const Icon(Icons.house),
+            title: const Text('ID domu'),
+            onTap: () async {
+              String houseId = await DatabaseService.getHouseId(uid);
+              showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                  title: const Text('ID domu',
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 165, 214, 167))),
+                  content: Text(houseId),
+                  actions: <Widget>[
+                    Center(
+                        child: TextButton(
+                      onPressed: () {
+                        Clipboard.setData(ClipboardData(text: houseId));
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Kopiuj do schowka',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 165, 214, 167))),
+                    )),
+                  ],
+                ),
+              );
             }),
       ],
     ));
