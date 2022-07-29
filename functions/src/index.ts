36728e6440e035e42f8b006ac1f30dd56a5e9b88
Darek <heniulu10@gmail.com>
2022-07-29 18:48:27 +0200

    fix(sendToDeviceOnListCreate): cloud function
    token wasnt being properly mapped, also needed to create an index in firebase for compound query that consisted of two .where: with == and !=

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,17 +10,14 @@ export const sendToDeviceOnListCreate= functions.firestore.document("houses/{hId
   const shoppingList=snap.data();
   const userWhoCreatedListSnapshot = await db.collection("users").doc(shoppingList.createdBy).get();
   const houseMembersSnapshot = await db.collection("users").where("houseId", "==", userWhoCreatedListSnapshot.data()!["houseId"]).where("userId", "!=", shoppingList.createdBy).get();
-
+  const tokens = houseMembersSnapshot.docs.map((snap) => snap.data()["token"]);
   const payload: admin.messaging.MessagingPayload = {
     notification: {
       title: "Nowa lista zakupów!",
-      body: `stworzona przez ${shoppingList.createdBy}`,
+      body: `stworzona przez ${userWhoCreatedListSnapshot.data()!["name"]}`,
       icon: "https://i.imgur.com/SGYPHRx.png",
       clickAction: "FLUTTER_NOTIFICATION_CLICK",
     },
   };
-  houseMembersSnapshot.forEach((houseMember) =>{
-    return fcm.sendToDevice(houseMember === null || houseMember === void 0 ? void 0 :
-        houseMember.data()["token"], payload);
-  });
+  return fcm.sendToDevice(tokens, payload);
 });
--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class DatabaseService {
